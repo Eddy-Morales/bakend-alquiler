2537bafdc21e2d234e8fd1d0984505b42a082226
Eddy_Morales <edyis1720@gmail.com>
2025-06-08 11:38:19 -0500

    correcciones

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -14,6 +14,7 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+
 const sendMailToRegister = (userMail, token) => {
 
     let mailOptions = {
@@ -32,6 +33,23 @@ const sendMailToRegister = (userMail, token) => {
             console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
         }
     })
+
+}
+
+const sendMailToRecoveryPassword = async(userMail,token)=>{
+    let info = await transporter.sendMail({
+    from: 'admin@vet.com',
+    to: userMail,
+    subject: "Correo para reestablecer tu contraseña",
+    html: `
+    <h1>SmartVET - 🐶 😺</h1>
+    <hr>
+    <a href=${process.env.URL_FRONTEND}reset/${token}>Clic para reestablecer tu contraseña</a>
+    <hr>
+    <footer>El equipo de SmartVET te da la más cordial bienvenida.</footer>
+    `
+    });
+    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
 }
 
 export {
