e54bdd6c63423a3081eba12feebb1b87cc306321
Eddy_Morales <edyis1720@gmail.com>
2025-08-04 12:43:36 -0500

    chat

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@
 import connection from './database.js';
 import app from './server.js'
 
-app.listen(app.get('port'),()=>{
-    console.log(`Server ok on http://localhost:${app.get('port')}`);
-})
+import http from 'http'
+import { Server } from 'socket.io'
+
+
 
 
 connection()
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: `https://panaroom.netlify.app`
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado',socket.id)
+    socket.on('enviar-mensaje-front-back',(payload)=>{
+        socket.broadcast.emit('enviar-mensaje-front-back',payload)
+    })
+})
+
+server.listen(app.get('port'),()=>{
+    console.log(`Server ok on http://localhost:${app.get('port')}`);
+})
